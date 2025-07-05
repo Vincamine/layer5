4dd39d482bcfd038255c25afac6811582c2765c5
Wyllie Fang <wylliefang@gmail.com>
2025-07-04 20:09:15 -0700

    save branch for Handbook fix try 1

--- src/sections/Community/Handbook/Handbook.style.js
+++ src/sections/Community/Handbook/Handbook.style.js
@@ -29,59 +29,48 @@ export const HandbookWrapper = styled.div`
         transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
       }
     }
-
-    h2 h3{
-      margin: 0.5rem 0;
-      color: ${(props) => props.theme.tertiaryColor};
-      transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-    }
-
     .page-section{
-      h2{
-        padding-top: 7rem;
-        margin-top: -7rem;
-      }
-      h3{
-        padding-top: 7rem;
-        margin-top: -7rem;
-      }
-      @media (min-width: 750px) {
-        margin-top: -36rem;
-        margin-left: 20rem;
-      }
-      @media (max-width: 749px) {
-        margin-top: 0;
-        margin-left: 0;
-        padding-top: 2rem;
-      }
-      display: flex;
+      // h2{
+      //   padding-top: 0rem;
+      //   margin-top: 0rem;
+      // }
+      // h3{
+      //   padding-top: 0rem;
+      //   margin-top: 0rem;
+      // }
+      // @media (min-width: 750px) {
+      //   margin-top: 0rem;
+      //   margin-left: 0rem;
+      // }
+      // @media (max-width: 749px) {
+      //   margin-top: 0rem;
+      //   margin-left: 0;
+      //   padding-top: 2rem;
+      // }
+      // display: flex
+      // flex-direction: column;
 
-      @media screen and (min-width: 768px) and (max-height: 1145px) {
-       margin-top : -62vh ;
-
-
-  }
-      @media screen and (min-width: 1280px) and (max-width: 1350px) {
-        margin-left:16rem;
-      }
+      // @media screen and (min-width: 768px) and (max-height: 1145px) {
+      //  margin-top : -62vh;
+      // }
+      // @media screen and (min-width: 1280px) and (max-width: 1350px) {
+      //   margin-left:16rem;
+      // }
     }
     .conduct-section{
       @media screen and (min-width: 751px) {
         margin-top:-43rem;
       }
             @media screen and (min-width: 768px) and (max-height: 1145px) {
-       margin-top : -66vh ! important;
-
+       margin-top : -66vh !important;
     }
-
   }
 
-    }
-    .page-section .heading-top{
-      @media screen and (min-width: 751px) {
-        padding-top:1rem;
-      }
-    }
+    // .page-section .heading-top{
+    //   @media screen and (min-width: 751px) {
+    //     padding-top:1rem;
+    //   }
+    // }
     .sidebar {
         margin: 0;
         padding: 0;
@@ -198,14 +187,15 @@ export const HandbookWrapper = styled.div`
       padding: 1rem 0;
       padding-bottom: 4rem;
       display: flex;
+      flex-direction: row !important;
       flex-wrap : wrap;
       align-items : center;
       justify-content: center;
     }
 
-    p{
-      margin-top: 10px;
-    }
+    // p{
+    //   margin-top: 10px;
+    // }
 
     input[type=checkbox] + label {
       display: block;
@@ -298,33 +288,33 @@ export const HandbookWrapper = styled.div`
       transform : translate(0,28%);
     }
 
-    @media only screen and (max-width: 750px){
-      .content > a:first-of-type > h2:first-of-type {
-        padding-top: 7rem;
-      }
-      .page-section{
-        margin-top: 0; !important
-        margin-left: 0; !important
-        padding: 1rem 2.5rem;
+    // @media only screen and (max-width: 750px){
+    //   .content > a:first-of-type > h2:first-of-type {
+    //     padding-top: 0em;
+    //   }
+    //   .page-section{
+    //     margin-top: 0; !important
+    //     margin-left: 0; !important
+    //     padding: 1rem 2.5rem;
       
-        .table-container {
-          table {
-            width: 100%;
-            margin-left: 0;
-          }
-          overflow-x: scroll;
-        }
+    //     .table-container {
+    //       table {
+    //         width: 100%;
+    //         margin-left: 0;
+    //       }
+    //       overflow-x: scroll;
+    //     }
     
-        .table-container::-webkit-scrollbar { 
-          display: none;
-        }
-      }
+    //     .table-container::-webkit-scrollbar { 
+    //       display: none;
+    //     }
+    //   }
       
-      .codes{
-        width:100%
-        margin-top:-2rem;
-      }
-    }
+    //   .codes{
+    //     width:100%
+    //     margin-top:-2rem;
+    //   }
+    // }
 
     @media only screen and (max-width: 475px){
       .page-header-section h1{
@@ -394,6 +384,7 @@ export const HandbookWrapper = styled.div`
 
     .channels-para {
       display: flex;
+      flex-direction: column;
       align-items: center;
     }
 
@@ -413,13 +404,13 @@ export const HandbookWrapper = styled.div`
     .heading{
       display: flex;
       flex-direction: column;
-      align-items: center;
+      // align-items: center;
     }
 
     .heading-start{
       display: flex;
-      flex-direction: row;
-      align-items: center;
+      flex-direction: column;
+      // align-items: center;
       justify-content: center;
       align-self: flex-end;
       margin-right: 2rem;
@@ -440,155 +431,156 @@ export const HandbookWrapper = styled.div`
       }
     }
 
-  .intra-page{
-    position: sticky;
-    top: 10rem;
-    right:0rem;
-    margin-right: 1rem;
-    padding-bottom: 5rem;
-    align-items:left;
-    justify-content:space-around;
-    display: flex;
-    flex-direction: column;
-    overflow:hidden;
-    .active{
-      font-weight:5000;
-      color: ${(props) => props.theme.secondaryColor};
-    }
-    ul{
-      list-style: none;
-      top: 3rem;
-    }
-    li{
-      padding-bottom:0.5rem;
-      padding-top:0.5rem;
-    }
-    @media only screen and (max-width: 900px){
-      width: 0;
-      opacity:0;
-    height:0;
-      transition:none;
-      visibility:hidden;
-    }
-  }
+      .intra-page{
+        position: sticky;
+        top: 10rem;
+        right:0rem;
+        margin-right: 1rem;
+        padding-bottom: 5rem;
+        align-items:left;
+        justify-content:space-around;
+        display: flex;
+        flex-direction: column;
+        overflow:hidden;
+        .active{
+          font-weight:5000;
+          color: ${(props) => props.theme.secondaryColor};
+        }
+        ul{
+          list-style: none;
+          top: 3rem;
+        }
+        li{
+          padding-bottom:0.5rem;
+          padding-top:0.5rem;
+        }
+        @media only screen and (max-width: 900px){
+          width: 0;
+          opacity:0;
+        height:0;
+          transition:none;
+          visibility:hidden;
+        }
+      }
 
-  .learn{
-    width: 10rem;
-  }
+      .learn{
+        width: 10rem;
+      }
 
 
-.writing_program{
-width: 90%;
-@media only screen and (max-width: 900px){
-  width: 100%;
-}
-.list{
-  padding: 1.875rem 0px;
-  table, tr, td{
-    border:none;
-   }
-  .text{
-    p{
-      color: ${props => props.theme.tertiaryColor};
-      transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-    }
-  }
-  .listed{
-      text-align: left;
-      .table{
-          .icon{
-              height: 2.5rem;
-              width: 2.5rem;
-              vertical-align: top;
-          }
-          .skill{
-              color: ${props => props.theme.tertiaryColor};
-              font-size: 16px;
-              padding: 0 0 1.25rem 1.8rem;
-              transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-              h4{
-                  font-weight: 600;
+    // .writing_program{
+    //   width: 90%;
+    //   @media only screen and (max-width: 900px){
+    //   width: 100%;
+    // }
+
+    .list{
+      padding: 1.875rem 0px;
+      table, tr, td{
+        border:none;
+      }
+      .text{
+        p{
+          color: ${props => props.theme.tertiaryColor};
+          transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+        }
+      }
+      .listed{
+          text-align: left;
+          .table{
+              .icon{
+                  height: 2.5rem;
+                  width: 2.5rem;
+                  vertical-align: top;
+              }
+              .skill{
+                  color: ${props => props.theme.tertiaryColor};
+                  font-size: 16px;
+                  padding: 0 0 1.25rem 1.8rem;
+                  transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+                  h4{
+                      font-weight: 600;
+                  }
               }
           }
       }
-  }
-}
-.content_list{
-  position: relative;
-  width: 100%;
-  margin-top: 1rem;
-  display: flex;
-  flex-wrap: wrap;
-}
-.content_type { 
-  display: flex;
-  flex-direction: column;
-  background: ${props => props.theme.grey212121ToWhite};
-  box-shadow: 0px 0px ${props => props.theme.projectShadowsize} ${props => props.theme.green00D3A9ToGreyE6E6E6};
-  transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-  &:hover{
-      box-shadow: 0px 0px 5px ${props => props.theme.primaryColor};
-  }
-  padding: 12% 6% 12% 6%;
-  height: 220px;
-  border-radius: 4%;
-  margin-bottom: 30px;
-  text-align: center;
-  h5{
-      margin-bottom: auto; 
-      margin-top: 0.5rem;
-      font-weight: 700;
-      color: ${props => props.theme.secondaryColor}
-  }
-  p{
-      font-weight: 300;
-      color: ${props => props.theme.text};
+    }
+    .content_list{
+      position: relative;
+      width: 100%;
+      margin-top: 1rem;
+      display: flex;
+      flex-wrap: wrap;
+    }
+    .content_type { 
+      display: flex;
+      flex-direction: column;
+      background: ${props => props.theme.grey212121ToWhite};
+      box-shadow: 0px 0px ${props => props.theme.projectShadowsize} ${props => props.theme.green00D3A9ToGreyE6E6E6};
       transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-  }
-  img{
-      height: 60px ; 
-      width: auto ;
-  }
-}
-.process {
-  margin: 10px auto;
-  border-left: solid 2px ${(props) => props.theme.tertiaryColor};
-  padding: 0px 20px 0px 20px;
-  transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-} 
-.process .item {
-  padding: 10px;
-  font-size: 16px;
-  line-height: 1.7;
-  position: relative;
-}
-.item::before {
-  content: '';
-  position: absolute;
-  width: 20px;
-  height: 20px;
-  display: block;
-  border: 3px solid ${(props) => props.theme.tertiaryColor};
-  border-radius: 50%;
-  background-color: ${(props) => props.theme.secondaryColor};
-  top: 25px;
-  left: -32px;
-  transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-}
-}
-/* repository overview  */
-div.accessRequired {
-  font-size: small;
-  line-height: .9rem;
-  font-style: italic;
-}
-details.invite-only {
-  margin: 1rem;
-  padding: 1rem;
-  background-color: ${(props) => props.theme.grey313131ToGreenC9FCF6};
-  span {
-    font-weight: bold;
-    display: inline;
-  }
-}
+      &:hover{
+          box-shadow: 0px 0px 5px ${props => props.theme.primaryColor};
+      }
+      padding: 12% 6% 12% 6%;
+      height: 220px;
+      border-radius: 4%;
+      margin-bottom: 30px;
+      text-align: center;
+      h5{
+          margin-bottom: auto; 
+          margin-top: 0.5rem;
+          font-weight: 700;
+          color: ${props => props.theme.secondaryColor}
+      }
+      p{
+          font-weight: 300;
+          color: ${props => props.theme.text};
+          transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+      }
+      img{
+          height: 60px ; 
+          width: auto ;
+      }
+    }
+    .process {
+      margin: 10px auto;
+      border-left: solid 2px ${(props) => props.theme.tertiaryColor};
+      padding: 0px 20px 0px 20px;
+      transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+    } 
+    .process .item {
+      padding: 10px;
+      font-size: 16px;
+      line-height: 1.7;
+      position: relative;
+    }
+    .item::before {
+      content: '';
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      display: block;
+      border: 3px solid ${(props) => props.theme.tertiaryColor};
+      border-radius: 50%;
+      background-color: ${(props) => props.theme.secondaryColor};
+      top: 25px;
+      left: -32px;
+      transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+    }
+    }
+    /* repository overview  */
+    div.accessRequired {
+      font-size: small;
+      line-height: .9rem;
+      font-style: italic;
+    }
+    details.invite-only {
+      margin: 1rem;
+      padding: 1rem;
+      background-color: ${(props) => props.theme.grey313131ToGreenC9FCF6};
+      span {
+        font-weight: bold;
+        display: inline;
+      }
+    }
 `;
